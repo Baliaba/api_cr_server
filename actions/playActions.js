a22068497ec6acf6a660377c906bda0b38d57c13
Hassan <valdezebaliaba@gmail.com>
2018-06-06 03:49:55 +0200

    Refund or optimize Player and battles actions

--- actions/playActions.js
+++ actions/playActions.js
@@ -4,36 +4,27 @@ var callApi = require('./../services/callServices.js');
 var clanAction = require('./../actions/clanActions.js');
 var battleHelpers = require('./../clanHelper/battleHelper.js');
 
-function getData(conf,schemas,refresh) {
-    let apiTest = false;
-     promise = new Promise((resolve,reject)=>{
+  getData = (conf,schemas,refresh,clanTag="") =>{
     // get players url after updated clans dats
-    clanAction.getData(conf, schemas)
+    clanAction.getData(conf, schemas,refresh,clanTag)
     .then((obj) => {
         let playerPromise = callApi.callapi(obj.urlPlay+conf.freeOfBattlesCardsPath, conf.params);
-        let battlePromise = callApi.callapi(obj.urlPlay+"/"+"battles"+conf.freeOfOpponent,conf.params);
+        let battlePromise = callApi.callapi(obj.urlPlay+"/battles"+conf.freeOfOpponent,conf.params);
         Promise.all([playerPromise,battlePromise])
         .then((responses)=>{
                 let playerData = responses[0];
                 let battleData = responses[1];
                 //Traitements
-                helpers.traitementPlayer(playerData, schemas).then((msg) => {
+                helpers.traitementPlayer(playerData, schemas).then((mg) => {
                     battleHelpers.TraitementBattle(battleData,conf,schemas,obj.clanTag)
                     .then((msg)=>{
-                        if(refresh === "off"){
+                        if(!refresh){
                             qeueu.removeQueue(conf,obj.key,obj.clanTag);
-                        }else{
-                            apiTest = true;
-                            resolve(msg);
                         }
                       })
                 })
         })
     })
-})
-if (apiTest){
-    return promise;
-}
 }
 
 var exports = module.exports = {
