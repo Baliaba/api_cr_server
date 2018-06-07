f60b3b3eedbf0e6bea5c67f1ffdd236cf4f1f027
Hassan <valdezebaliaba@gmail.com>
2018-06-08 01:55:28 +0200

    make some tests

--- server_test.js
+++ server_test.js
@@ -7,19 +7,19 @@ var
     conf = require('./config/conf.js'),
     CronJob = require('cron').CronJob,
     router = require('./router/router.js'),
-    loop = require('./actions/playActions'),
+    loop = require('./actions/clanActions.js'),
     schemas = require('./models/schemas.js');
     queue = require('./services/queueService.js');
-job = new CronJob('*/1 * * * *', () => {
-        loop.getData(conf, schemas,refresh=false);
-    }, () => { /* CronJob every 2 seconds...*/
-        console.log("Job stopped");
-    },
-    true,
-    "Europe/Paris"
-);
+// job = new CronJob('*/1 * * * *', () => {
+//         loop.getData(conf, schemas,refresh=false);
+//     }, () => { /* CronJob every 2 seconds...*/
+//         console.log("Job stopped");
+//     },
+//     true,
+//     "Europe/Paris"
+// );
 
-//loop.getData(conf, schemas,refresh=true,"88CC09J");
+loop.getData(conf, schemas,refresh=false)
 app.use('/api', router.router);
 app.listen(conf.PORT);
 //job.start();
